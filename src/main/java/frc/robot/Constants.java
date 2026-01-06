d193209c2a35c082360d4c37e2e6ca47d3eb81c2
Devin Kovacs <deviweviwoohoo@gmail.com>
2026-01-06 17:37:17 -0600

    first commit, preparing for season start DK

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import swervelib.math.Matter;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  public static class OperatorConstants {
+    public static final CommandJoystick PRIMARY = new CommandJoystick(1);
+    public static final CommandXboxController SECONDARY = new CommandXboxController(0);
+
+    // Joystick Deadband
+    public static final double DEADBAND = 0.15;
+    public static final double LEFT_Y_DEADBAND = 0.15;
+    public static final double RIGHT_X_DEADBAND = 0.15;
+    public static final double TURN_CONSTANT = 6;
+  }
+
+  public static final class DrivebaseConstants {
+
+    // Hold time on motor brakes when disabled
+    public static final double WHEEL_LOCK_TIME = 10; // seconds
+  }
+
+  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+  public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag
+  public static final double MAX_SPEED = Units.feetToMeters(14.5);
+  // Maximum speed of the robot in meters per second, used to limit acceleration.
+  public static final double ARM_ANGLE = 53.0; // angle between straight up arm and placing arm
+
+  // all this stuff is important I think?
+  public static final double X_REEF_ALIGNMENT_P = 3.3;
+  public static final double Y_REEF_ALIGNMENT_P = 3.3;
+  public static final double ROT_REEF_ALIGNMENT_P = 0.058;
+
+  public static final double ROT_SETPOINT_REEF_ALIGNMENT = 0; // Rotation
+  public static final double ROT_TOLERANCE_REEF_ALIGNMENT = 1;
+  public static final double X_SETPOINT_REEF_ALIGNMENT = -0.34; // Vertical pose
+  public static final double X_TOLERANCE_REEF_ALIGNMENT = 0.02;
+  public static final double Y_SETPOINT_REEF_ALIGNMENT = 0.16; // Horizontal pose
+  public static final double Y_TOLERANCE_REEF_ALIGNMENT = 0.02;
+
+  public static final double DONT_SEE_TAG_WAIT_TIME = 1;
+  public static final double POSE_VALIDATION_TIME = 0.3;
+
+  public static final class SubsystemConstants {
+    // not drivebase constants
+    public static final int ELEVMOT1ID = 20;
+    public static final int ELEVMOT2ID = 21;
+    public static final int GAMEPIECE_SENSOR_ID = 0;
+    public static final int CLAW_ID = 2;
+    public static final int ARM_ID = 22;
+    public static final double SPIN_SPEED = 0.05; // placeholder value
+  }
+}
